6b5a01272f97c498decdd319009ec13f374af969
MacBook <VladDB@github.com>
2022-01-27 20:56:04 +0300

    one more fix after review

--- src/main/java/hexlet/code/schemas/BaseSchema.java
+++ src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -2,6 +2,7 @@ package hexlet.code.schemas;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class BaseSchema {
@@ -10,14 +11,14 @@ public class BaseSchema {
 
     //добавление правила для проверки
     public final void addRule(Predicate<Object> rule) {
-        rules.add(rule);
+        rules.add(rule.and(Objects::nonNull));
     }
 
     //валидация данных
     public final boolean isValid(Object data) {
         for (Predicate<Object> validator: rules) {
 
-            if (data == null || !validator.test(data)) {
+            if (!validator.test(data)) {
                 return false;
             }
         }
